3c6b6afc6539ffad5ca11ceea1187d88cd278ffa
jcmayoral <jocamaba1989@gmail.com>
2017-10-16 14:24:23 +0200

    footprint_checker modifications and update of changelog collision checker changes to footprint extender... costmap utilities removed

--- unmerged/footprint_checker/ros/include/footprint_checker/footprint_extender.h
+++ unmerged/footprint_checker/ros/include/footprint_checker/footprint_extender.h
@@ -24,7 +24,7 @@
 #include <footprint_checker/CollisionCheckerMsg.h>
 #include <costmap_2d/costmap_2d.h>
 
-class CollisionChecker
+class FootprintExtender
 {
     public:
         /**
@@ -33,45 +33,19 @@ class CollisionChecker
          *
          * @param base_footprint vector containing vertices of the footprint
          */
-        CollisionChecker();
+        FootprintExtender();
 
-        CollisionChecker(ros::NodeHandle &nh);
+        FootprintExtender(ros::NodeHandle &nh);
         /**
          * Dtor.
          */
-        virtual ~CollisionChecker();
-
-        /**
-         * This method performs time parameterization of a given trajectory.
-         *
-         * @param costmap stores current costmap
-         * @param target_pose_in stores given target pose
-         * @return True if base does not in collision
-         */
-        bool isBaseInCollision(geometry_msgs::Polygon polygon);
+        virtual ~FootprintExtender();
 
         /*
         * Calculte intermediate footprints between vertices
         */
         void getIntermediateFootprint(geometry_msgs::Polygon polygon);
 
-        /*
-        * Check target footprint cell
-        * @return true if not collision detected
-        */
-        bool checkCells();
-
-        /*
-        * Convert Map from Ooccupancy Grid to Costmap_2d class
-        */
-
-        void convertMap(const nav_msgs::OccupancyGrid& costmap);
-
-
-        /**
-         * Copy ctor.
-         */
-        CollisionChecker(const CollisionChecker &other);
 
         /*
         * Store base_frame
@@ -87,31 +61,9 @@ class CollisionChecker
     private:
 
         /*
-        true is map is already converted
-        */
-        bool convert_map_;
-
-        /**
-        * Store Costmap
-        */
-        costmap_2d::Costmap2D costmap_;
-
-        /*
-        * Select the probability for an occupied cell
-        */
-        double threshold_;
-
-        /*
         * Store Scalling factor for number of footprint point
         */
         int scaling_factor_new_footprint_;
-
-        /*
-        */
-        int max_number_of_vertices_in_collision_;
-
-
-        geometry_msgs::Quaternion collided_orientations_;
 
 };
 
---
+++ unmerged/footprint_checker/ros/src/footprint_extender.cpp
@@ -0,0 +1,71 @@
+/**
+ * Copyright [2016] <Bonn-Rhein-Sieg University>
+ * base_collision_checker_node.cpp
+ *
+ * Created on: May 16th, 2016
+ * Author: Jose Carlos Mayoral
+ */
+#include <footprint_checker/footprint_extender.h>
+#include <ros/ros.h>
+#include <math.h>
+using namespace std;
+
+FootprintExtender::FootprintExtender(){
+
+}
+
+
+FootprintExtender::FootprintExtender(ros::NodeHandle &nh)
+{
+    nh.param("base_frame", base_frame_, std::string("map"));
+    nh.param("scalling_factor", scaling_factor_new_footprint_,3);
+}
+
+
+FootprintExtender::~FootprintExtender()
+{
+
+}
+
+void FootprintExtender::getIntermediateFootprint(geometry_msgs::Polygon polygon){
+
+	  int i = 0;
+    int initial_points = polygon.points.size();
+
+    footprint_extended_vector_.clear();
+
+    //foot_print_vertices_pub_
+    geometry_msgs::PoseArray footprint_vertices;
+    footprint_vertices.header.frame_id = base_frame_;
+
+    while( i < (initial_points))
+    {
+        double diffx = 0.0;
+        double diffy = 0.0;
+        int cycle = 1;
+
+        if (i < initial_points-1){
+            diffx = (polygon.points[i+1].x -
+                polygon.points[i].x)/scaling_factor_new_footprint_;
+
+            diffy = (polygon.points[i+1].y -
+                polygon.points[i].y)/scaling_factor_new_footprint_;
+        }
+        else{
+            diffx = (polygon.points[0].x -
+                polygon.points[i].x)/scaling_factor_new_footprint_;
+            diffy = (polygon.points[0].y -
+                polygon.points[i].y)/scaling_factor_new_footprint_;
+        }
+
+        while (cycle <= scaling_factor_new_footprint_){
+            double x = polygon.points[i].x + (diffx*cycle);
+            double y = polygon.points[i].y + (diffy*cycle);
+            footprint_extended_vector_.push_back(std::make_pair(x,y));
+            cycle++;
+        }
+
+    	// add third row 1
+        i += 1;
+    }
+}
