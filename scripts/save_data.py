ba08a2c5affc0c31d1cd0bb36d8d787d8b5c53fe
jcmayoral <jocamaba1989@gmail.com>
2017-11-25 12:13:16 +0100

    rosbag script automated

--- scripts/save_data.py
+++ scripts/save_data.py
@@ -2,29 +2,27 @@ import rosbag
 import rospy
 from geometry_msgs.msg import AccelStamped, Twist
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, LaserScan
 
 
 class BagRecorder():
     def __init__(self):
         self.bag = rosbag.Bag('test.bag', 'w')
-        rospy.Subscriber("accel", AccelStamped, self.accCB)
-        rospy.Subscriber("cmd_vel", Twist, self.cmdCB)
-        rospy.Subscriber("odom", Odometry, self.odomCB)
+        rospy.Subscriber("/accel", AccelStamped, self.mainCB, "/accel")
+        rospy.Subscriber("/cmd_vel", Twist, self.mainCB, "/cmd_vel")
+        rospy.Subscriber("/odom", Odometry, self.mainCB, "/odom")
+        rospy.Subscriber("/scan_front", LaserScan, self.mainCB, "/scan_front") 
+        rospy.Subscriber("/arm_cam3d/rgb/image_raw", Image, self.mainCB, "/arm_cam3d/rgb/image_raw")
         rospy.loginfo("Initializing")
 
     def writeToBag(self,topic, msgs):
         self.bag.write(topic, msgs)
         
-    def odomCB(self,msg):
-        self.writeToBag("odom", msg)
-
-    def accCB(self,msg):
-        self.writeToBag("accel", msg)
-    
-    def cmdCB(self,msg):
-        self.writeToBag("cmd_vel", msg)
-    
+    def mainCB(self,msg, topic_name):
+        self.writeToBag(topic_name, msg)    
+        
     def close(self):
+        rospy.loginfo("Closing Bag File")
         self.bag.close()
     
 rospy.init_node("my_bag_recorder")
