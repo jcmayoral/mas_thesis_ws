e3cf6e1d98a63dd56561f359a6f4d9d74a0ab561
jcmayoral <jocamaba1989@gmail.com>
2017-11-25 11:57:23 +0100

    rosbag_recorder python script

--- scripts/save_data.py
+++ scripts/save_data.py
@@ -1,22 +1,36 @@
 import rosbag
 import rospy
-import sys
-from std_msgs.msg import Int32, String
+from geometry_msgs.msg import AccelStamped, Twist
+from nav_msgs.msg import Odometry
 
-bag = rosbag.Bag('test.bag', 'w')
 
+class BagRecorder():
+    def __init__(self):
+        self.bag = rosbag.Bag('test.bag', 'w')
+        rospy.Subscriber("accel", AccelStamped, self.accCB)
+        rospy.Subscriber("cmd_vel", Twist, self.cmdCB)
+        rospy.Subscriber("odom", Odometry, self.odomCB)
+        rospy.loginfo("Initializing")
+
+    def writeToBag(self,topic, msgs):
+        self.bag.write(topic, msgs)
+        
+    def odomCB(self,msg):
+        self.writeToBag("odom", msg)
+
+    def accCB(self,msg):
+        self.writeToBag("accel", msg)
+    
+    def cmdCB(self,msg):
+        self.writeToBag("cmd_vel", msg)
+    
+    def close(self):
+        self.bag.close()
+    
+rospy.init_node("my_bag_recorder")
+bagRecord = BagRecorder()
 
 while not rospy.is_shutdown():
-  try:
-    str = String()
-    str.data = 'foo'
+    rospy.spin()
 
-    i = Int32()
-    i.data = 42
-
-    bag.write('chatter', str)
-    bag.write('numbers', i)
-  except:
-    print "Unexpected error:", sys.exc_info()[0]
- 
-bag.close()
+bagRecord.close()
