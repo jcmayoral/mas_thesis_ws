4e501c122f6b01896924fe393f69123fdded0f3d
Jose Carlos Mayoral Banos <jose@smail.inf.h-brs.de>
2017-11-29 13:35:09 +0100

    scripts rosbag modifications problem error detected anyways

--- scripts/save_data.py
+++ scripts/save_data.py
@@ -3,17 +3,20 @@ import rospy
 from geometry_msgs.msg import AccelStamped, Twist
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, LaserScan
-
+import sys
 
 class BagRecorder():
-    def __init__(self):
-        self.bag = rosbag.Bag('test.bag', 'w')
+    def __init__(self,file_name):
+        self.bag = rosbag.Bag(file_name +'.bag', 'w')
         rospy.Subscriber("/accel", AccelStamped, self.mainCB, "/accel")
         rospy.Subscriber("/cmd_vel", Twist, self.mainCB, "/cmd_vel")
-        rospy.Subscriber("/odom", Odometry, self.mainCB, "/odom")
+	rospy.Subscriber("/odom", Odometry, self.mainCB, "/odom")
+        rospy.Subscriber("/base/odometry_controller/odom", Odometry, self.mainCB, "/base/odometry_controller/odom")
         rospy.Subscriber("/scan_front", LaserScan, self.mainCB, "/scan_front") 
         rospy.Subscriber("/scan_rear", LaserScan, self.mainCB, "/scan_rear") 
+        rospy.Subscriber("/scan_unified", LaserScan, self.mainCB, "/scan_unified") 
         rospy.Subscriber("/arm_cam3d/rgb/image_raw", Image, self.mainCB, "/arm_cam3d/rgb/image_raw")
+        rospy.Subscriber("/cam3d/rgb/image_raw", Image, self.mainCB, "/cam3d/rgb/image_raw")
         rospy.loginfo("Initializing")
 
     def writeToBag(self,topic, msgs):
@@ -27,7 +30,13 @@ class BagRecorder():
         self.bag.close()
     
 rospy.init_node("my_bag_recorder")
-bagRecord = BagRecorder()
+
+file_name = 'test'
+
+if len(sys.argv) > 1:
+  file_name=str(sys.argv[1])
+
+bagRecord = BagRecorder(file_name)
 
 while not rospy.is_shutdown():
     rospy.spin()
