114aa2669618c2816f481b0ec94a6390aebfbe41
jcmayoral <jocamaba1989@gmail.com>
2017-12-27 20:01:23 -0600

    issue write_bag_sm solved

--- scripts/bag_writer_state_machine.py
+++ scripts/bag_writer_state_machine.py
@@ -1,7 +1,7 @@
 import rosbag
 import rospy
 import smach
-from smach_ros import SimpleActionState
+from smach_ros import SimpleActionState, IntrospectionServer
 import roslib
 import actionlib
 from actionlib_msgs.msg import GoalStatus
@@ -33,7 +33,7 @@ class MyBagRecorder(smach.State):
     def __init__(self):
         self.busy = False
         self.bag = rosbag.Bag('starter.bag', 'w')
-        #self.is_finished = False
+        self.is_finished = False
         rospy.Subscriber("/accel", AccelStamped, self.mainCB, "/accel")
         rospy.Subscriber("/cmd_vel", Twist, self.mainCB, "/cmd_vel")
         rospy.Subscriber("/base/twist_mux/command_navigation", Twist, self.mainCB, "/base/twist_mux/command_navigation")
@@ -61,23 +61,25 @@ class MyBagRecorder(smach.State):
         if userdata.restart_requested:
             print ("NEW FILE")
             self.close()
-            self.busy = True # Filter error of cb when file is already closed
+            self.is_finished = True # Filter error of cb when file is already closed
             self.bag = rosbag.Bag(userdata.shared_string + str(userdata.counter_in) +'.bag', 'w')
             #userdata.counter_out = userdata.counter_in + 1
-            self.busy = False # End Filter
+            self.is_finished = False # End Filter
             userdata.restart_requested_out = False
             return "RECORD_STARTED"
 
         if userdata.restart_requested is None:
             print ("EXITING")
+            self.is_finished = True
             self.close()
             return "END_RECORD"
 
     def writeToBag(self,topic, msgs):
-        while (self.busy is True):
+        while (self.busy):
             pass
         self.busy = True
-        self.bag.write(topic, msgs)
+        if not self.is_finished:
+            self.bag.write(topic, msgs)
         self.busy = False
 
     def mainCB(self,msg, topic_name):
@@ -86,7 +88,7 @@ class MyBagRecorder(smach.State):
     def close(self):
         rospy.loginfo("Closing Bag File")
         self.bag.close()
-        #self.is_finished = True
+        self.is_finished = True
 
 rospy.init_node("my_bag_recorder")
 
@@ -123,12 +125,12 @@ with sm:
             return 'succeeded'
 
     smach.StateMachine.add('SETUP', Setup(),
-                       transitions={'SETUP_DONE':'START_WRITER', 'FINISH_REQUEST': 'START_WRITER'},
+                       transitions={'SETUP_DONE':'CONFIG_WRITER', 'FINISH_REQUEST': 'CONFIG_WRITER'},
                        remapping={'counter_in':'sm_counter',
                                   'counter_out':'sm_counter',
                                   'restart_requested_out':'restart_requested'})
 
-    smach.StateMachine.add('START_WRITER', MyBagRecorder(),
+    smach.StateMachine.add('CONFIG_WRITER', MyBagRecorder(),
                    transitions={'RECORD_STARTED':'TRIGGER_MOVE', 'END_RECORD': 'END_SM'},
                    remapping={'counter_in':'sm_counter',
                               'counter_out':'sm_counter',
@@ -146,4 +148,10 @@ with sm:
                       transitions={'succeeded':'SETUP', 'aborted':'SETUP'})
 
 #bagRecord.close()
+#Instrospection
+sis = IntrospectionServer('bag_writer', sm, '/SM_ROOT')
+sis.start()
+# Execute the state machine
 outcome = sm.execute()
+# Wait for ctrl-c to stop the application
+sis.stop()
