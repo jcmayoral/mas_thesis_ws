d35ca3de0e9af2656407712794456f9c4bfdc66c
jcmayoral <jocamaba1989@gmail.com>
2018-01-25 11:36:33 +0100

    odom_monitoring nnode created

---
+++ other_sources/odom_monitoring/src/odom_monitoring_tools/Plotter.py
@@ -0,0 +1,26 @@
+import rospy
+from MyStatics.RealTimePlotter import RealTimePlotter
+from nav_msgs.msg import Odometry
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+class Plotter(RealTimePlotter):
+    def __init__(self, threshold = 1000, pace = 200):
+        self.data_ = []
+        self.step_ = []
+        print ("Plotter Constructor Initialized")
+        RealTimePlotter.__init__(self,threshold,pace,True)
+        #self.ax.legend("True")
+        rospy.init_node("odom_plotter", anonymous=True)
+        rospy.Subscriber("/base/odometry_controller/odometry", Odometry, self.amclCB)
+        plt.show()
+        rospy.spin()
+        plt.close("all")
+
+    def amclCB(self, msg):
+        data = [msg.twist.twist.linear.x,msg.twist.twist.linear.y,msg.twist.twist.angular.z]
+
+        self.step_.append(msg.header.seq)
+        self.data_.append(data)
+        self.update(msg.header.seq,self.step_,self.data_)
