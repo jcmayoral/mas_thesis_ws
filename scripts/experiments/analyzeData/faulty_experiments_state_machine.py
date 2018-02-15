4554b925ad08e80d7c07bc7ecc2c5896659fd34a
jcmayoral <jocamaba1989@gmail.com>
2018-02-15 13:41:07 +0100

    collision analyzeData script created

---
+++ scripts/experiments/analyzeData/faulty_experiments_state_machine.py
@@ -0,0 +1,450 @@
+#!/usr/bin/env python
+"""
+This state machine is designed to read an "n" number of rosbag files given
+a common string name on such a way that all rosbag files must share that name
+followed by a number, i.e. "test" with rosbag files "test1.bag", "test2.bag"...
+"testX.bag".
+
+Once a rosbag file had finished the next is executed.
+
+Created by: Jose Mayoral
+"""
+#import roslib; roslib.load_manifest('smach_tutorials')
+from __future__ import print_function
+import rospy
+import smach
+import smach_ros
+import rosbag
+import math
+from geometry_msgs.msg import AccelStamped, Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Image, LaserScan, Imu
+from std_msgs.msg import Empty, String, Header
+from fusion_msgs.msg import sensorFusionMsg
+from dynamic_reconfigure.client import Client
+import numpy as np
+import matplotlib.pyplot as plt
+
+# define state ReadBag
+class MyBagReader(smach.State):
+    def __init__(self):
+        mytypes = [AccelStamped, Twist, Odometry, Odometry, LaserScan, LaserScan, LaserScan, Image, Image, Odometry, Header, Imu]
+        #self.path = '/home/jose/ROS/thesis_ws/my_ws/rosbag_test/cob3/static_runs_2911/static_runs/' #TODO
+        #self.path = '/home/jose/ROS/thesis_ws/my_ws/rosbag_test/cob3/cob3-test-2301/'
+        self.path = '/home/jose/data/'
+
+        self.mytopics = ["/accel", "/cmd_vel", "/odom", "/base/odometry_controller/odom",
+            "/scan_front", "/scan_rear", "/scan_unified",
+            "/arm_cam3d/rgb/image_raw","/cam3d/rgb/image_raw", "/base/odometry_controller/odometry", "/collision_label", "/imu/data"]
+
+        self.myPublishers = list()
+
+        self.finish_pub = rospy.Publisher("finish_reading", String, queue_size=1)
+
+        for topic_name, msg_type in zip(self.mytopics,mytypes):
+            publisher = rospy.Publisher(topic_name, msg_type, queue_size=1)
+            self.myPublishers.append([publisher,topic_name])
+
+        smach.State.__init__(self,
+                             outcomes=['RESTART_READER','END_READER'],
+                             input_keys=['foo_counter_in', 'shared_string'],
+                             output_keys=['foo_counter_out'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state Reader')
+
+        self.is_file_ok = False
+        max_bag_file = 50
+
+        while not self.is_file_ok:
+            try:
+                file_name = self.path + userdata.shared_string + str(userdata.foo_counter_in)+".bag"
+                self.bag = rosbag.Bag(file_name)
+                self.is_file_ok = True
+            except:
+                userdata.foo_counter_out = userdata.foo_counter_in + 1
+
+                if userdata.foo_counter_in > max_bag_file:
+                    fb = String()
+                    fb.data = "END_BAG"
+                    userdata.foo_counter_out = 1
+                    self.finish_pub.publish(fb)
+                    rospy.sleep(2)
+                    return 'END_READER'
+                #print("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+
+        try:
+            start_time = self.bag.get_start_time()
+            end_time = self.bag.get_end_time()
+            duration_time = end_time - start_time
+            r = rospy.Rate(100) # Default 100
+            for topic, msg, t in self.bag.read_messages(topics=self.mytopics):
+                print ("ROSBag  Running ", t.to_sec() - start_time, " of " , duration_time, end="\r")
+                for p, topic_name in self.myPublishers:
+                    if topic_name == topic:
+                        #print "printing on ", topic_name
+                        p.publish(msg)
+                        r.sleep()
+                        break
+            print ("\n")
+            self.bag.close()
+        except:
+            rospy.logwarn("ROSBAG FILE NOT FOUND")
+            self.bag.close()
+
+        if userdata.foo_counter_in < max_bag_file:  #n number of bag files // TODO default 35
+            userdata.foo_counter_out = userdata.foo_counter_in + 1
+            fb = String()
+            fb.data = "NEXT_BAG"
+            self.finish_pub.publish(fb)
+            rospy.sleep(2)
+            return 'RESTART_READER'
+        else:
+            fb = String()
+            fb.data = "END_BAG"
+            userdata.foo_counter_out = 1
+            self.finish_pub.publish(fb)
+            rospy.sleep(2)
+            return 'END_READER'
+
+class RestartReader(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['NEXT_BAG'],
+                             input_keys=['bar_counter_in'])
+        #rospy.spin()
+        rospy.sleep(0.2)
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing state RESTART READER')
+        #rospy.loginfo('Counter = %f'%userdata.bar_counter_in)
+        monitor_reset_pub = rospy.Publisher('/sm_reset', Empty, queue_size=1)
+        while monitor_reset_pub.get_num_connections() < 1:
+            pass
+        #print (monitor_reset_pub.get_num_connections())
+        monitor_reset_pub.publish(Empty())
+        #print ("Send EMPTY")
+        rospy.sleep(0.5)
+        return 'NEXT_BAG'
+
+class Setup(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['SETUP_DONE', 'FINISH'],
+                             input_keys=['counter_in','acc_results', 'cam_results', 'odom_results', 'imu_results', 'result_cum', 'results_', 'x_array'],
+                             output_keys=['counter_out','acc_results', 'cam_results', 'odom_results', 'imu_results', 'result_cum', 'results_', 'x_array'])
+        #rospy.spin()
+        self.acc_client = Client("accelerometer_process", timeout=3, config_callback=self.callback)
+        self.cam_client = Client("vision_utils_ros", timeout=3, config_callback=self.callback)
+        self.odom_client = Client("odom_collisions", timeout=3, config_callback=self.callback)
+        self.imu_client = Client("imu_detector", timeout=3, config_callback=self.callback)
+        self.is_first_time = True
+
+        rospy.sleep(0.2)
+
+
+    def callback(self,config):
+        #print (config)
+        pass
+        #rospy.loginfo("Config set to {double_param}, {int_param}, {double_param}, ".format(**config))
+
+    def execute(self, userdata):
+        rospy.loginfo('Executing SETUP')
+        self.acc_client.update_configuration({"window_size": 20})#userdata.counter_in})
+        self.acc_client.update_configuration({"reset": True})
+        self.odom_client.update_configuration({"window_size": 20})#userdata.counter_in})
+        self.odom_client.update_configuration({"reset": True})
+        self.imu_client.update_configuration({"window_size": 20})#userdata.counter_in})
+        self.imu_client.update_configuration({"reset": True})
+
+        #SURF Version
+        self.cam_client.update_configuration({"matching_threshold":  userdata.counter_in * 0.01})
+        self.cam_client.update_configuration({"mode": 0})
+        self.cam_client.update_configuration({"reset": True})
+
+        rospy.sleep(0.5)
+        if self.is_first_time:#userdata.counter_in < 75: # Window SIZe Define max TODO
+            userdata.x_array.append(userdata.counter_in)
+            userdata.counter_out = userdata.counter_in + 5
+            self.is_first_time = False
+            return 'SETUP_DONE'
+        else:
+            userdata.results_['accel'] = userdata.acc_results
+            userdata.results_['cam'] = userdata.cam_results
+            userdata.results_['odom'] = userdata.odom_results
+            userdata.results_['imu'] = userdata.imu_results
+            return 'FINISH'
+
+class Plotter(smach.State):
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['PLOT_DONE'],
+                             input_keys=['data_in', 'x_array'])
+    def execute(self, userdata):
+        rospy.loginfo('Executing SETUP')
+
+        #Accelerometer Plot
+        plt.figure()
+        data = np.array(userdata.data_in['accel'])
+        x_array = userdata.x_array
+        plt.plot(x_array,data[:,0], label='Accelerometer Threshold X')
+        plt.plot(x_array,data[:,1], label='Accelerometer Threshold Y')
+        plt.plot(x_array,data[:,2], label='Accelerometer Threshold Z')
+        plt.xlabel("Window Size")
+        plt.ylabel("Maximum Threshold Detected")
+        plt.legend()
+        plt.title('Accelerometer on Motion Thresholding') # subplot 211 title
+        plt.savefig('accelerometer_motion_threshold.png') # TODO
+
+        #Camera Plot
+        plt.figure()
+        data = np.array(userdata.data_in['cam'])
+        plt.plot(np.asarray(x_array) * 0.01, data[:,0], label='RGB Camera Thresholds')
+        plt.xlabel("Matching Threshold Variation")
+        plt.ylabel("Maximum Threshold Detected")
+        plt.title('Camera on Motion Thresholding') # subplot 211 title
+        plt.legend()
+        plt.savefig('camera_motion_threshold.png') # TODO
+
+        plt.figure()
+        data = np.array(userdata.data_in['odom'])
+        x_array = userdata.x_array
+        plt.plot(x_array,data[:,0], label='Odometry Threshold X')
+        plt.plot(x_array,data[:,1], label='Odometry Threshold Y')
+        plt.plot(x_array,data[:,2], label='Odometry Threshold Z')
+        plt.xlabel("Window Size")
+        plt.ylabel("Maximum Threshold Detected")
+        plt.legend()
+        plt.title('Odometry Speeds on Motion Thresholding') # subplot 211 title
+        plt.savefig('odometry_motion_threshold.png') # TODO
+
+        plt.figure()
+        data = np.array(userdata.data_in['imu'])
+        x_array = userdata.x_array
+        plt.plot(x_array,data[:,0], label='Linear Acceleration X')
+        plt.plot(x_array,data[:,1], label='Linear Acceleration Y')
+        plt.plot(x_array,data[:,2], label='Linear Acceleration Z')
+        plt.plot(x_array,data[:,3], label='Angular Velocity X')
+        plt.plot(x_array,data[:,4], label='Angular Velocity Y')
+        plt.plot(x_array,data[:,5], label='Angular Velocity Z')
+        plt.xlabel("Window Size")
+        plt.ylabel("Maximum Threshold Detected")
+        plt.legend()
+        plt.title('IMU Speeds on Motion Thresholding') # subplot 211 title
+        plt.savefig('imu_motion_threshold.png') # TODO
+
+        f = open( 'file', 'w' )
+        for key, value in userdata.data_in.iteritems():
+        #for item in userdata.data_in:
+            f.write("'{0}'/n".format(key))
+            f.write("'{0}'/n".format(value))
+        f.close()
+        #plt.show()
+        rospy.sleep(0.5)
+        return 'PLOT_DONE'
+
+# define state Monitor
+class Monitor(smach.State):
+    def __init__(self, experiment_type):
+        smach.State.__init__(self,
+                             outcomes=['NEXT_MONITOR', 'END_MONITOR'],
+                              input_keys=['acc_cum', 'cam_cum', 'odom_cum', 'imu_cum', 'result_cum'],
+                              output_keys=['acc_cum', 'cam_cum', 'odom_cum', 'imu_cum', 'result_cum'])
+        rospy.Subscriber("/finish_reading", String, self.fb_cb)
+        rospy.Subscriber("/collision_label", Header, self.header_cb)
+
+        self.current_counter = 0
+        self.accel_thr = list()
+        self.cam_thr = list()
+        self.odom_thr = list()
+        self.imu_thr = list()
+
+        self.acc_cum = list()
+        self.cam_cum = list()
+        self.odom_cum = list()
+        self.imu_cum = list()
+
+
+        if experiment_type is "collisions_counter":
+            for i in range(10):
+                rospy.Subscriber("/collisions_"+str(i), sensorFusionMsg, self.counter_cb, queue_size=100)
+        if experiment_type is "threshold_calculator":
+            for i in range(10):
+                rospy.Subscriber("/collisions_"+str(i), sensorFusionMsg, self.threshold_cb, queue_size=100)
+
+    def threshold_cb(self,msg):
+        if msg.sensor_id.data == "acc_1":
+            self.accel_thr.append(msg.data)
+        if msg.sensor_id.data == "cam_0":
+            self.cam_thr.append(msg.data)
+        if msg.sensor_id.data == "odom":
+            self.odom_thr.append(msg.data)
+        if msg.sensor_id.data == "imu_1":
+            self.imu_thr.append(msg.data)
+
+    def counter_cb(self,msg):
+        if msg.msg == 2:
+            print (end="\n")
+            rospy.logwarn("collision_detected")
+            self.current_counter = self.current_counter + 1
+
+    def fb_cb(self,msg):
+        #print ("CB", msg)
+        self.next_bag_request = True
+        self.stop_bag_request = False
+
+        if msg.data == "NEXT_BAG":
+            self.current_counter = 0
+        else:#FINISH
+            #print ("current_counter" , self.current_counter)
+            print ("accel_thr" , np.nanmax(self.accel_thr, axis=0) , " number of samples " , len(self.accel_thr))
+            print ("cam_thr" , np.nanmax(self.cam_thr, axis=0), " number of samples " , len(self.cam_thr))
+            print ("odom_thr" , np.nanmax(self.odom_thr, axis=0), " number of samples " , len(self.odom_thr))
+            print ("imu_thr" , np.nanmax(self.imu_thr, axis=0), " number of samples " , len(self.imu_thr))
+
+            self.stop_bag_request = True
+
+    def header_cb(self,msg):
+        print (end="\n")
+        rospy.logerr("GROUND TRUTH: %s", msg.stamp.secs)
+
+    def execute(self, userdata):
+
+        self.next_bag_request = False
+        rospy.loginfo('Executing state MONITORING')
+        #rospy.sleep(20)#TODO
+
+        while not self.next_bag_request:
+            pass #TODO
+
+        rospy.sleep(0.2)
+        self.next_bag_request = False
+
+        if self.stop_bag_request:
+            userdata.acc_cum.append(np.nanmax(self.accel_thr, axis=0))
+            userdata.cam_cum.append(np.nanmax(self.cam_thr, axis=0))
+            userdata.odom_cum.append(np.nanmax(self.odom_thr, axis=0))
+            userdata.imu_cum.append(np.nanmax(self.imu_thr, axis=0))
+
+            del self.accel_thr[:]
+            del self.cam_thr[:]
+            del self.odom_thr[:]
+            del self.imu_thr[:]
+            return 'END_MONITOR'
+        else:
+            #print ("NEXT_MONITOR")
+            return 'NEXT_MONITOR'
+
+def monitor_cb(ud, msg):
+    return None
+
+def main():
+
+    rospy.init_node('smach_example_state_machine')
+
+    # Create a SMACH state machine
+    sm = smach.StateMachine(outcomes=['END_SM'])
+    sm.userdata.window_size = 2
+    #sm.userdata.bag_family = "cob3-attempt-2001-" #TODO
+    sm.userdata.window_size_array = list()
+    sm.userdata.results_ = dict()
+    sm.userdata.acc_results = list()
+    sm.userdata.cam_results = list()
+    sm.userdata.odom_results = list()
+    sm.userdata.imu_results = list()
+
+    reading_sm = smach.StateMachine(outcomes=['END_READING_SM'])
+    reading_sm.userdata.sm_counter = 1
+    reading_sm.userdata.bag_family = "collision_bags_bags"#"cob3-attempt-2301-" #TODO
+
+    with reading_sm:
+        smach.StateMachine.add('RESET_READING', RestartReader(),
+                       transitions={'NEXT_BAG':'READING'},
+                       remapping={'bar_counter_in':'sm_counter'})
+        smach.StateMachine.add('READING', MyBagReader(),
+                               transitions={'RESTART_READER':'RESET_READING',
+                                            'END_READER':'END_READING_SM'},
+                               remapping={'foo_counter_in':'sm_counter',
+                                          'shared_string':'bag_family',
+                                          'foo_counter_out':'sm_counter'})
+
+    monitoring_sm = smach.StateMachine(outcomes=['END_MONITORING_SM'])
+    monitoring_sm.userdata.results_ = sm.userdata.results_
+    monitoring_sm.userdata.acc_results = sm.userdata.acc_results
+    monitoring_sm.userdata.cam_results = sm.userdata.cam_results
+    monitoring_sm.userdata.odom_results = sm.userdata.odom_results
+    monitoring_sm.userdata.imu_results = sm.userdata.imu_results
+
+    #montoring_sm.userdata.window_size_array = sm.window_size_array
+    experiment_type = "collisions_counter" #TODO
+    #experiment_type = "threshold_calculator" #TODO
+
+    with monitoring_sm:
+        smach.StateMachine.add('WAIT_FOR_READER', smach_ros.MonitorState("/sm_reset", Empty, monitor_cb),
+                                transitions={'invalid':'MONITOR', 'valid':'WAIT_FOR_READER', 'preempted':'WAIT_FOR_READER'})
+        smach.StateMachine.add('MONITOR', Monitor(experiment_type),
+                       transitions={'NEXT_MONITOR':'WAIT_FOR_READER', 'END_MONITOR':'END_MONITORING_SM'},
+                       remapping={'result_cum':'results_',
+                                  'acc_cum':'acc_results',
+                                  'cam_cum':'cam_results',
+                                  'odom_cum': 'odom_results',
+                                  'imu_cum': 'imu_results',
+                                  })
+
+    # Open the container
+    with sm:
+        smach.StateMachine.add('SETUP', Setup(),
+                       transitions={'SETUP_DONE':'CON', 'FINISH': 'PLOT_RESULTS'},
+                       remapping={'counter_in':'window_size',
+                                  'counter_out':'window_size',
+                                  'result_cum':'results_',
+                                  'acc_cum':'acc_results',
+                                  'cam_cum':'cam_results',
+                                  'odom_cum':'odom_results',
+                                  'imu_cum': 'imu_results',
+                                  'x_array': 'window_size_array'})
+
+        #Concurrent
+        sm_con = smach.Concurrence(outcomes=['END_CON'],
+                                   default_outcome='END_CON',
+                                   outcome_map={#'RESTART':
+                                       #{ 'MONITORING_SM':'RESTART_MONITOR',
+                                         #'READ_SM' : 'RESTART_READER'},
+                                         'END_CON':
+                                         {'READ_SM': 'END_READING_SM',
+                                          'MONITORING_SM': 'END_MONITORING_SM'}})
+        # Open the container
+        with sm_con:
+            # Add states to the container
+            #smach.Concurrence.add('WAIT_MONITOR', smach_ros.MonitorState("/sm_reset", Empty, monitor_cb))
+            smach.Concurrence.add('READ_SM', reading_sm)
+            smach.Concurrence.add('MONITORING_SM', monitoring_sm)
+
+        #sm.userdata.data_in = monitoring_sm.userdata.acc_results #TODO
+        sm.userdata.data_in = monitoring_sm.userdata.results_ #TODO
+
+        smach.StateMachine.add('CON', sm_con,
+                       transitions={#'RESTART':'CON',
+                                    'END_CON':'SETUP'})
+        smach.StateMachine.add('PLOT_RESULTS', Plotter(),
+                       transitions={'PLOT_DONE':'END_SM'},
+                       remapping={'data_in': 'data_in',
+                                  'x_array': 'window_size_array'})
+
+    # Execute SMACH plan
+    #rospy.sleep(10)
+    #outcome = sm.execute()
+    #rospy.spin()
+
+    #Instrospection
+    sis = smach_ros.IntrospectionServer('server_name', sm, '/SM_ROOT')
+    sis.start()
+    # Execute the state machine
+    outcome = sm.execute()
+    # Wait for ctrl-c to stop the application
+    plt.show()
+    sis.stop()
+
+if __name__ == '__main__':
+    main()
