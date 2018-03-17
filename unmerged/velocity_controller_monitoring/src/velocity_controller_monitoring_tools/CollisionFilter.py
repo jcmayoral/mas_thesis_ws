6798099c0cca36b0aee9a409dd8138103f7fa919
Jose Carlos Mayoral Banos <jose@smail.inf.h-brs.de>
2018-03-17 17:14:05 +0100

    velocity controller removing stuff

--- unmerged/velocity_controller_monitoring/src/velocity_controller_monitoring_tools/CollisionFilter.py
+++ unmerged/velocity_controller_monitoring/src/velocity_controller_monitoring_tools/CollisionFilter.py
@@ -9,7 +9,7 @@ from velocity_controller_monitoring.cfg import filterConfig
 
 
 class CollisionFilter(ChangeDetection):
-    def __init__(self, cusum_window_size = 10, threshold = 10 ):
+    def __init__(self, threshold = 10 ):
         self.data_ = []
         self.data_.append([0,0,0])
         self.step_ = []
@@ -20,14 +20,12 @@ class CollisionFilter(ChangeDetection):
         self.msg = 0
         self.current_data = Twist()
         self.frame = 'test'
-        self.window_size = cusum_window_size
         self.is_disable = False
-        self.callBackFunction = self.updateChangeDetectionData
         ChangeDetection.__init__(self,3)
         rospy.init_node("controller_cusum", anonymous=True)
         self.openLoop_ = Twist()
         self.closeLoop_ = Twist()
-        self.pub = rospy.Publisher('filter', controllerFusionMsg, queue_size=10)
+        self.pub = rospy.Publisher('filter', controllerFusionMsg, queue_size=1)
         self.dyn_reconfigure_srv = Server(filterConfig, self.dynamic_reconfigureCB)
         rospy.Subscriber("/base/twist_mux/command_navigation", Twist, self.openLoopCB)
         rospy.Subscriber("/base/odometry_controller/odometry", Odometry, self.closeLoopCB)
@@ -35,17 +33,7 @@ class CollisionFilter(ChangeDetection):
 
     def dynamic_reconfigureCB(self,config, level):
         self.threshold = config["threshold"]
-        self.window_size = config["window_size"]
         self.is_disable = config["is_disable"]
-
-        if config["reset"]:
-            self.clear_values()
-            config["reset"] = False
-
-        if config["mode"]:
-            self.callBackFunction = self.updateThreshold
-        else:
-            self.callBackFunction = self.updateChangeDetectionData
 
         return config
 
@@ -56,16 +44,6 @@ class CollisionFilter(ChangeDetection):
                 np.fabs(self.current_data.angular.z)]
         self.publishMsg(data)
 
-    def updateChangeDetectionData(self,msg):
-        print ("Change Detection")
-        self.addData([self.current_data.linear.x, self.current_data.linear.y, self.current_data.angular.z])
-
-        if ( len(self.samples) > self.window_size):
-            self.samples.pop(0)
-
-        self.changeDetection(len(self.samples))
-        cur = np.array(self.cum_sum, dtype = object)
-        self.publishMsg(cur)
 
     def openLoopCB(self, msg):
         self.openLoop_ = msg
@@ -76,21 +54,21 @@ class CollisionFilter(ChangeDetection):
         self.current_data.linear.y = self.openLoop_.linear.y - self.closeLoop_.linear.y
         self.current_data.angular.z = self.openLoop_.angular.z - self.closeLoop_.angular.z
 
-        self.callBackFunction(msg)
+        self.updateThreshold(msg)
 
 
     def publishMsg(self,data):
         output_msg = controllerFusionMsg()
         #Filling Message
         output_msg.header.frame_id = self.frame
-        output_msg.window_size = self.window_size
+        output_msg.window_size = 1
         #print ("Accelerations " , x,y,z)
         output_msg.controller_id.data = self.controller_id
         output_msg.header.stamp = rospy.Time.now()
+        print (data)
 
         if any(abs(t) > self.threshold for t in data):
             rospy.logwarn("IGNORE")
-            print (data)
             output_msg.mode = controllerFusionMsg.IGNORE
         if not self.is_disable:
             self.pub.publish(output_msg)
